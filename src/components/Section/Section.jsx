0b0bd8a46d17058d8fa86e759bb41967862d11b6
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-12-10 18:28:56 +0200

    first commit before check

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import section from '../Section/Section.module.css';
+
+class Section extends Component {
+  static props = {
+    title: this.title,
+    children: this.children,
+  };
+  render() {
+    const { title, children } = this.props;
+
+    return (
+      <section className={section.section}>
+        <h1>{title}</h1>
+        {children}
+      </section>
+    );
+  }
+}
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export { Section };
